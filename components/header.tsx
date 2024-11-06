3e5c2a9f198b08844f4aaa0ffb5932daf92ca345
Saniyakanwal <160118062+Saniyakanwal@users.noreply.github.com>
2024-11-06 23:19:57 +0500

    figma design

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='bg-[#A29875] h-[134px]'>
+      <h1 className='text-[75px]  font-serif pt-5 pl-9 h-[94px] w-[439px] text-white'>MANZZARI</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ components/hero.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="h-screen">
+    <div className="flex h[80%]">
+    <div className="w-1/2 flex flex-col justify-center items-start m-5">
+      <h1 className="text-[40px] w-[496px]  mt-[3l6px] ml-44 ">IMPECCABLE CRAFTSMANSHIP AND FINESSE</h1>
+      <p className='text-2xl mt-2 ml-44 w-[600px] text-[rgb(120,112,84)]'>An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
+      <button className='text-3xl bg-[#A29875] w-72 h-14 mt-7 rounded-[10px] ml-44 gap-[10px] text-white'>Explore Now</button>
+    </div>
+    <div className="w-1/2 flex mt-[198px] ml-24 justify-center items-center">
+      <Image src="/Images/figma.png" alt="pic" width={462} height={647} />
+    </div>
+   </div>
+   </div>
+  )
+}
+
+export default Hero
