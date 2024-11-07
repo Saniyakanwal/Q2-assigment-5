f0155bf715d1e11790382a97203ab9354f415b70
Saniyakanwal <160118062+Saniyakanwal@users.noreply.github.com>
2024-11-07 23:20:34 +0500

    figma design updated

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <p className='text-2xl mt-2 ml-44 w-[600px] text-[rgb(120,112,84)]'>An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
       <button className='text-3xl bg-[#A29875] w-72 h-14 mt-7 rounded-[10px] ml-44 gap-[10px] text-white'>Explore Now</button>
     </div>
-    <div className="w-1/2 flex mt-[198px] ml-24 justify-center items-center">
+    <div className="w-1/2 flex mt-[80px] ml-24 justify-center items-center">
       <Image src="/Images/figma.png" alt="pic" width={462} height={647} />
     
     </div>
